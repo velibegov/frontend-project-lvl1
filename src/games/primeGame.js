1d3b75971586a6a9679060cfdd97c8d724bf1857
RVelibegov <velibegov@mail.ru>
2022-04-02 15:36:18 +0300

    fix bug with 1 is prime too :D

--- src/games/primeGame.js
+++ src/games/primeGame.js
@@ -9,6 +9,9 @@ export function getGameData() {
 }
 
 const isPrime = (number) => {
+  if (number === 1) {
+    return true;
+  }
   for (let i = 2; i < number; i += 1) {
     if (number % i === 0) {
       return false;
